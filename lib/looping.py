3606ed2d569a415f875897371e363a031ab2b60d
Victor Kimathi <vkimathi97@gmail.com>
2025-06-17 23:27:57 +0300

    python-p3-loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    counter = 10
+    while counter > 0:
+        print(counter)
+        counter -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz") 
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
